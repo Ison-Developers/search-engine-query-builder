63980a4444c2c621cf98753eb7248870975a0304
Iman Mohamadi <turbosianor@gmail.com>
2014-06-20 20:43:15 +0430

    a schema with different placeholdrs added for testing placeholder parameter test

---
+++ src/schemas/especialSolrSchema.js
@@ -0,0 +1,16 @@
+define([], function () {
+  var schema = {
+    query: {
+      types: {
+        and: 'q=AND(-.-)',
+        or: 'q=OR(-.-)',
+        regular: 'q=(-.-)'
+      }
+    },
+    pageSize: 'rows=(-.-)',
+    startPage: 'start=(-.-)'
+  };
+
+  return schema;
+});
+
